15416f68f5fae25f155c5b53469c43cb5fdb05ac
Ogg (Valentin M. Padurean) <912518+padurean@users.noreply.github.com>
2018-05-27 22:38:22 +0300

    Show tag page in index.html if tag query param is present

--- main.js
+++ main.js
@@ -9,7 +9,21 @@ var state = {
   btnOlderJqElem: null,
   btnNewerJqElem: null
 }
-function renderPostsList() {
+function indexOf(search, arr) {
+  return arr.indexOf ? arr.indexOf(search) : $.inArray(search, arr);
+}
+function postToHtml(post) {
+  var date = new Date(post.date).toLocaleString();
+  var postPath = state.postsPath + post.htmlFileName;
+  var postHtml =
+    '\t<article class="post-summary">\n' +
+    '\t\t<a href="' + postPath + '"><h2 id="post-title" class="post-title">' + post.title + '</h2></a>\n' +
+    '\t\t<time id="post-date" class="post-date" datetime="' + post.date + '">' + date + '</time>\n' +
+    '\t\t<p>' + post.description + '</p>\n' +
+    '\t</article>';
+  return postHtml;
+}
+function renderPostsPage() {
   var postsHtmlArr = ['\n'];
   for (
     var i = (state.currPage-1) * state.pageSize;
@@ -17,23 +31,28 @@ function renderPostsList() {
     i++
   ) {
     var post = state.posts[i][1];
-    var date = new Date(post.date).toLocaleString();
-    var postPath = state.postsPath + post.htmlFileName;
-    var postHtml =
-      '\t<article class="post-summary">\n' +
-      '\t\t<a href="' + postPath + '"><h2 id="post-title" class="post-title">' + post.title + '</h2></a>\n' +
-      '\t\t<time id="post-date" class="post-date" datetime="' + post.date + '">' + date + '</time>\n' +
-      '\t\t<p>' + post.description + '</p>\n' +
-      '\t</article>\n';
-    postsHtmlArr.push(postHtml);
+    postsHtmlArr.push(postToHtml(post));
   }
-  state.postsJqElem.html(postsHtmlArr.join(''));
+  state.postsJqElem.html(postsHtmlArr.join('\n'));
+}
+function renderPostsForTag(tag) {
+  var postsHtmlArr = [ '\n\t<span>Posts with tag</span> <a>'+tag+'</a>:' ];
+  for (var i = 0; i < state.posts.length; i++) {
+    var post = state.posts[i][1];
+    if (indexOf(tag, post.tags) >= 0)
+      postsHtmlArr.push(postToHtml(post));
+  }
+  postsHtmlArr.push(
+    '\t<br><a href="#" class="btn" onclick="window.history.go(-1); return false;">' +
+    '<span class="btn-arrow-icon">&laquo;</span> Go Back</a><br>\n'
+  );
+  state.postsJqElem.html(postsHtmlArr.join('\n'));
 }
 function goToCurrentPage() {
   var newUrl = state.currPage > 1 ? '?page='+state.currPage : '/';
   if (history.pushState) {
     window.history.pushState('', '', newUrl)
-    renderPostsList();
+    renderPostsPage();
   } else {
     window.location = newUrl;
   }
@@ -73,20 +92,33 @@ $(function(){
     state.postsJqElem = $('#posts');
     state.btnOlderJqElem = $('#btn-older');
     state.btnNewerJqElem = $('#btn-newer');
-    if (state.nbPages > 1) {
-      var queryParamsArr = location.search.replace('?', '').split('=');
-      if (queryParamsArr.length >= 2) {
-        var indexOfPage = queryParamsArr.indexOf ?
-          queryParamsArr.indexOf('page') :
-          $.inArray('page', queryParamsArr);
-        if (indexOfPage >= 0 && indexOfPage < queryParamsArr.length-1)
-          state.currPage = parseInt(queryParamsArr[indexOfPage + 1]);
+    var tag = null;
+    var queryParamsArr = location.search.replace('?', '').split('=');
+    if (queryParamsArr.length >= 2) {
+      // tags view
+      var indexOfTag = indexOf('tag', queryParamsArr);
+      if (indexOfTag >= 0 && indexOfTag < queryParamsArr.length-1) {
+        tag = queryParamsArr[indexOfTag + 1];
+      }
+      // posts view
+      else {
+        if (state.nbPages > 1) {
+          var indexOfPage = indexOf('page', queryParamsArr);
+          if (indexOfPage >= 0 && indexOfPage < queryParamsArr.length-1)
+            state.currPage = parseInt(queryParamsArr[indexOfPage + 1]);
+        }
       }
     }
-    state.btnOlderJqElem.on('click', goToNextPageClb);
-    state.btnNewerJqElem.on('click', goToPrevPageClb);
-    toggleNextPrevPageBtns();
-    renderPostsList();
+    if (!tag) {
+      renderPostsPage();
+      state.btnOlderJqElem.on('click', goToNextPageClb);
+      state.btnNewerJqElem.on('click', goToPrevPageClb);
+      toggleNextPrevPageBtns();
+    } else {
+      state.btnOlderJqElem.hide();
+      state.btnNewerJqElem.hide();
+      renderPostsForTag(tag);
+    }
     $('#year-placeholder').text(new Date().getFullYear());
   });
 });
