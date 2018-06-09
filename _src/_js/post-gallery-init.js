4c5bf765a0463c90af8cb43aae085080b4df6748
Ogg (Valentin M. Padurean) <912518+padurean@users.noreply.github.com>
2018-06-09 11:13:35 +0300

    - Fix close icon positioning on mobile devices for featherlight gallery - Make gallery transition between images slightly faster

--- _src/_js/post-gallery-init.js
+++ _src/_js/post-gallery-init.js
@@ -5,7 +5,7 @@ $(function(){
     targetAttr: 'src',
     previousIcon: '&#9001;'/*'&#9664;'*/,     /* Code that is used as previous icon */
     nextIcon: '&#9002;'/*'&#9654;'*/,         /* Code that is used as next icon */
-    galleryFadeIn: 300,          /* fadeIn speed when slide is loaded */
-    galleryFadeOut: 300          /* fadeOut speed before slide is loaded */
+    galleryFadeIn: 200,          /* fadeIn speed when slide is loaded */
+    galleryFadeOut: 200          /* fadeOut speed before slide is loaded */
   });
 });
